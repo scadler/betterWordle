f8182f7d6bbfb1f31873ef86457c575d2882be67
scadler <scotthadler01@gmail.com>
2022-02-03 05:16:02 +0000

    adding codeword generator function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("works")
+var i = 0
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "U", "W", "X", "Y", "Z"]
+var codeWord = ""
+while(i<=3){
+    codeWord += alphabet[Math.floor(Math.random())]
+    console.log(codeWord)
+    i++
+}
