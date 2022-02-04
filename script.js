3fd174f625f3aca4f14db41cb276300a4c6eb8c5
scadler <scotthadler01@gmail.com>
2022-02-04 02:45:29 +0000

    added input checker

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("works")
 var i = 0
 var alphabet = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
 var letterList = "";
@@ -16,4 +15,10 @@ while(i<=3){
         $("#outputDebug").text(codeWord);
     }
 }
-
+$("#debugButton").click(function(){
+    var guess = $("#guessOne").val().toUpperCase();
+    console.log(guess)
+    if(guess === codeWord){
+        console.log("works")
+    }
+});
